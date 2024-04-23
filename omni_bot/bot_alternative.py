ca000c20a93582a30aaa0b5eb420bf9e78e404da
Raebel Chisto <raebel.christo@gmail.com>
2024-04-23 15:47:09 +0530

    axis flip

--- omni_bot/bot_alternative.py
+++ omni_bot/bot_alternative.py
@@ -65,9 +65,9 @@ class Robot:
         vel1 = clamp((get_speed(theta1) + rotation)*speed, -1, 1)
         vel2 = clamp((get_speed(theta2) + rotation)*speed, -1, 1)
         vel3 = clamp((get_speed(theta3) + rotation)*speed, -1, 1)
-        self.controller.set_pwm(RIGHT, mapping(vel1, -1, 1, RIGHT_MIN_PULSE, RIGHT_MAX_PULSE))
-        self.controller.set_pwm(LEFT, mapping(vel2, -1, 1, LEFT_MIN_PULSE, LEFT_MAX_PULSE))
-        self.controller.set_pwm(REAR, mapping(vel3, -1, 1, REAR_MIN_PULSE, REAR_MAX_PULSE))
+        self.controller.set_pwm(RIGHT, mapping(vel1, -1, 1, RIGHT_MAX_PULSE, RIGHT_MIN_PULSE))
+        self.controller.set_pwm(LEFT, mapping(vel2, -1, 1, LEFT_MAX_PULSE, LEFT_MIN_PULSE))
+        self.controller.set_pwm(REAR, mapping(vel3, -1, 1, REAR_MAX_PULSE, REAR_MIN_PULSE))
 
     def halt(self):
         self.controller.set_pwm(RIGHT, 0)
